6383b302a18645105c2679263dd75d87172e3903
Shreyas <spapinwar@gmail.com>
2026-01-18 14:58:09 +0530

    Initial Commit

---
+++ Onera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Onera
+//
+//  Created by shreyas on 18/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Onera/OneraApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneraApp.swift
+//  Onera
+//
+//  Created by shreyas on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct OneraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
