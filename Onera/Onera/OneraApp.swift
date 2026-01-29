8143d283c8a4e8a095b1fb783f682fe37ed11320
Shreyas Papinwar <spapinwar@gmail.com>
2026-01-29 19:43:47 +0800

    Adding new multiplatform app

---
+++ Onera/Onera/OneraApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneraApp.swift
+//  Onera
+//
+//  Created by Sangeeta Papinwar on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct OneraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
